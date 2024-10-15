06f807d58e081de1a77c7847cf723ab6c5348540
OGBram <156800524+OGBram@users.noreply.github.com>
2024-10-15 13:23:36 -0700

    3

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("hello world");
+const canvas = document.getElementById("canvasMain");
+const ctx = canvas.getContext("2d");
+canvas.height = 2560;
+canvas.width = 2560;
+ctx.fillStyle = "white";
+ctx.font = "100px monospace";
+
+var lastTime;
+    var requiredElapsed = 1000 / 50; 
+
+    requestAnimationFrame(loop);
+
+    function loop(now) {
+        requestAnimationFrame(loop);
+
+        if (!lastTime) { lastTime = now; }
+        var elapsed = now - lastTime;
+
+        if (elapsed > requiredElapsed) {
+            ctx.fillText("Hello World", 50, 100);
+            lastTime = now;
+        }
+    }
